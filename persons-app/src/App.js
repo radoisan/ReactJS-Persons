67af5ad8a61176a5fe8b64e0f8ad17acf9151279
Radoslav Mihajlov <radoisan@gmail.com>
2019-10-22 18:26:23 +0300

    Debbuging React Apps - Error Boundaries
    Using Error Boundaries(React 16+)
    ErrorBoundary is higher oreder component. Use ErrorBoundaries when we know that some important code may fail.

--- persons-app/src/App.js
+++ persons-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 import classes from './App.module.css';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -61,12 +62,13 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
+            return <ErrorBoundary key={person.id} >
+              <Person 
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age} 
-              key={person.id} 
               changed={(event) => this.nameChangeHandler(event, person.id)} />
+              </ErrorBoundary>
           })} 
         </div>
       );
