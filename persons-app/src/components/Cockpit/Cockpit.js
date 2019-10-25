91ecf21ebcbac4bca2583be5578e24356d152b53
Radoslav Mihajlov <radoisan@gmail.com>
2019-10-25 14:14:28 +0300

    Optimizing Functional Components with React.memo()
    Class components can bail out from rendering when their input props are the same using PureComponent or shouldComponentUpdate. Now you can do the same with function components by wrapping them in React.memo.

--- persons-app/src/components/Cockpit/Cockpit.js
+++ persons-app/src/components/Cockpit/Cockpit.js
@@ -32,10 +32,10 @@ const cockpit = (props) => {
         btnClass = classes.Red;    
     }
 
-    if (props.persons.length <= 2){
+    if (props.personsLenght <= 2){
       assignedClasses.push(classes.red)
     }
-    if (props.persons.length <= 1){
+    if (props.personsLenght <= 1){
       assignedClasses.push(classes.bold)
     }
 
@@ -50,4 +50,4 @@ const cockpit = (props) => {
     );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
--- persons-app/src/containers/App.js
+++ persons-app/src/containers/App.js
@@ -102,7 +102,7 @@ class App extends Component {
           <Cockpit 
           title={this.props.appTitle}
           showPersons={this.state.showPersons}
-          persons={this.state.persons} 
+          personsLenght={this.state.persons.length} 
           clicked={this.togglePersonsHandler} 
           />
         ):null }
