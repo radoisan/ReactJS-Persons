cb2ea54b1f2cd998eccddce19455e2b834baf3ac
Radoslav Mihajlov <radoisan@gmail.com>
2019-10-28 01:51:50 +0200

    Setting State Correctly
    Using optional syntax. In nameChangeHandler added anonimous arrow func to setState(), which receives prevState and props as arguments, and in the anonimous function body we need to return new state object. By using prevState React guarantee that prevState would be as we expect it to be. Best practice for state updates, that depends on the old state !!!

--- persons-app/src/containers/App.js
+++ persons-app/src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     ],
     otherState: 'Some other state',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state){
@@ -60,8 +61,13 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons})
-  }
+    this.setState( (prevState, props) => {
+      return {
+        persons: persons, 
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;  
