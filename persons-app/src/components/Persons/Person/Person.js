30964c85bccc61bd5f5c4e119fc9fa40cace336d
Radoslav Mihajlov <radoisan@gmail.com>
2019-10-27 21:22:43 +0200

    Rendering Adjacent JSX Elements
    Created Higher Order Component (hoc) folder. Created Auxiliary.js file and created func which accepts props and return props.children. And use <Aux> </Aux> component as wraper, when we don't wanna render extra DOM element , as html div element or other.

--- persons-app/src/components/Persons/Person/Person.js
+++ persons-app/src/components/Persons/Person/Person.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+
+import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.module.css'
-
-
+/*<div className={classes.Person} >*/
+/*</div>*/
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person} >
+        <Aux>
+            
                 <p onClick={this.props.click}>I'm {this.props.name} and {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
+            
+        </Aux>
         );
     }
 }
