f1ddf67616f974fbcf774163f22f76674ced193f
Radoslav Mihajlov <radoisan@gmail.com>
2019-10-27 21:28:26 +0200

    Using React.Fragment
    React.Freagment is introduced in newer versions of React, and technically  do exactly the same work as Aux component. A common pattern in React is for a component to return multiple elements. Fragments let you group a list of children without adding extra nodes to the DOM.

--- persons-app/src/components/Persons/Person/Person.js
+++ persons-app/src/components/Persons/Person/Person.js
@@ -8,13 +8,13 @@ class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
         return (
-        <Aux>
+        <React.Fragment>
             
                 <p onClick={this.props.click}>I'm {this.props.name} and {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}></input>
             
-        </Aux>
+        </React.Fragment>
         );
     }
 }
