704e6903432569bd5aef933155f650bb26c4c31b
Radoslav Mihajlov <radoisan@gmail.com>
2019-10-28 02:36:24 +0200

    Using Refs
    Refs provide a way to access DOM nodes or React elements created in the render method. Two ways of adding a reference.
    
    https://reactjs.org/docs/refs-and-the-dom.html

--- persons-app/src/components/Persons/Person/Person.js
+++ persons-app/src/components/Persons/Person/Person.js
@@ -8,15 +8,35 @@ import classes from './Person.module.css'
 /*<div className={classes.Person} >*/
 /*</div>*/
 class Person extends Component {
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+    
+    componentDidMount(){
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render(){
         console.log('[Person.js] rendering...');
         return (
         <Aux>
-            
-                <p onClick={this.props.click}>I'm {this.props.name} and {this.props.age} years old</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            
+
+            <p onClick={this.props.click}>
+                I'm {this.props.name} and {this.props.age} years old
+            </p>
+                
+            <p key='i2'>{this.props.children}</p>
+
+            <input 
+                key='i3'
+                //ref={(inputEl) => {this.inputElement = inputEl}}
+                ref={this.inputElementRef}
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name} 
+            />
         </Aux>
         );
     }
