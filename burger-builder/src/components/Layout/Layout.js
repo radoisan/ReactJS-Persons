5bf8c63feb54cc40b1ff815b630e56ed70769ba4
Radoslav Mihajlov <radoisan@gmail.com>
2019-11-02 01:22:28 +0200

    Reusing the Backdrop

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <Sidedrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar  />
+                <Sidedrawer 
+                closed={this.sideDrawerClosedHandler}
+                open={this.state.showSideDrawer} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+
+export default Layout;
